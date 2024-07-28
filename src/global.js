2a5c7e04e6b94a947bfec15bfa91978bd5293bab
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-07-28 01:21:01 -0700

    Add better commment to Udemy URL regex pattern

--- src/global.js
+++ src/global.js
@@ -5,10 +5,13 @@ const DEFAULT_CHECKBOX_VALUE = true;
 const MIN_SPEED = 0.5;
 const MAX_SPEED = 4;
 
-// This will activate the extension if it matches the following
-// https://www.udemy.com/course/<foo>/learn/lecture/<bar>
-// foo = name of the course
-// bar = is the id of the lecture
+/** This will activate the extension if it matches the following
+ * https://www.udemy.com/course/foo/learn/lecture/bar
+ *
+ * foo = name of the course
+ *
+ * bar = is the id of the lecture
+ */
 const UDEMY_VIDEO_URL_PATTERN =
   /^https:\/\/www\.udemy\.com\/course\/[^\/]+\/learn\/lecture\/[^\/]+$/;
 
