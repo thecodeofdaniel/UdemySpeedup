c1fe6e019e8882beb66406cd2c1dce35ca14e3b8
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-10-19 10:03:41 -0700

    Add UI options for slider and shortcut step

--- src/popup/js/settings.js
+++ src/popup/js/settings.js
@@ -1,32 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const minSpeedInput = document.getElementById('minSpeed');
-  const maxSpeedInput = document.getElementById('maxSpeed');
-  const saveButton = document.getElementById('saveSettings');
+  const minSpeedInputElem = document.getElementById('minSpeed');
+  const maxSpeedInputElem = document.getElementById('maxSpeed');
+  const saveButtonElem = document.getElementById('saveSettings');
 
-  // Load current settings
+  // Load current settings to inputs
   browser.storage.local.get([MIN_SPEED_KEY, MAX_SPEED_KEY]).then((result) => {
-    minSpeedInput.value = result.minSpeed || 0.1;
-    maxSpeedInput.value = result.maxSpeed || 16;
+    minSpeedInputElem.value = result[MIN_SPEED_KEY] || 0.5;
+    maxSpeedInputElem.value = result[MAX_SPEED_KEY] || 4;
   });
 
-  // Save settings
-  saveButton.addEventListener('click', () => {
-    const minSpeed = parseFloat(minSpeedInput.value);
-    const maxSpeed = parseFloat(maxSpeedInput.value);
+  // Save settings if valid
+  saveButtonElem.addEventListener('click', () => {
+    const minSpeed = parseFloat(minSpeedInputElem.value);
+    const maxSpeed = parseFloat(maxSpeedInputElem.value);
 
-    if (minSpeed >= 0.1 && maxSpeed <= 16 && minSpeed < maxSpeed) {
-      // browser.storage.local.set({ minSpeed, maxSpeed }).then(() => {
-      //   alert('Settings saved successfully!');
-      //   window.close();
-      // });
+    if (
+      minSpeed &&
+      maxSpeed &&
+      minSpeed >= 0.0625 &&
+      maxSpeed <= 16 &&
+      minSpeed < maxSpeed
+    ) {
       LSset(MIN_SPEED_KEY, minSpeed);
       LSset(MAX_SPEED_KEY, maxSpeed);
-
       alert('Settings saved successfully!');
-      console.log('minSpeed:', minSpeed);
-      console.log('maxSpeed:', maxSpeed);
     } else {
-      alert('Invalid input. Please ensure min speed is less than max speed and both are between 0.1 and 16.');
+      alert(
+        'Invalid input. Please ensure min speed is less than max speed and both are between 0.0625 and 16.',
+      );
     }
   });
 });
