900eca4a50047b8f40b0988eab6f427405a27b9e
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-12-19 22:22:15 -0800

    Update video and playback css selectors

--- src/content_scripts/0_selectors.js
+++ src/content_scripts/0_selectors.js
@@ -1,8 +1,8 @@
 // What's this?: These are css selectors to select the elements needed.
 
 // Selectors
-const VIDEO_SELECTOR = 'video';
-const PLAYBACK_TEXT_SELECTOR = 'span.ud-focus-visible-target';
+const VIDEO_SELECTOR = "video[class*='video-player--video-player']";
+const PLAYBACK_TEXT_SELECTOR = "span[class*='playback-rate--trigger-text']";
 const PROGRESS_BAR_SELECTOR = '.progress-bar--slider--z064U';
 const NEXT_BUTTON_SELECTOR = '#go-to-next-item';
 const PLAYBACK_POPUP_SELECTOR = '.playback-rate--menu--4b1Qm';
